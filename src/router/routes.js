87a107eb0b7dfcee20636d1462e4fd4d06916493
kyusueng.sim <ksshim86@gmail.com>
2021-05-26 19:20:24 +0900

    # path 수정

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,14 +1,14 @@
 const routes = [
   {
     path: '/',
-    component: () => import('pages/Index.vue')
+    component: () => import('../App.vue')
     // children: [
     //   { path: 'welcome', component: () => import('pages/Welcome.vue') },
     //   { path: 'todo', component: () => import('pages/Welcome.vue') }
     // ]
   },
   { path: '/welcome', component: () => import('pages/Welcome.vue') },
-  { path: '/todo', component: () => import('pages/TodoPage.vue'), props: { scrollHeight: 1 } },
+  { path: '/todo', component: () => import('pages/TodoPage.vue') },
   // Always leave this as last one,
   // but you can also remove it
   {
