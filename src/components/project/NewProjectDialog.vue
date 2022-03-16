639a73164425cfead18d23d5827a44a49cce85e1
ksshim86 <ksshim86@gmail.com>
2022-03-17 05:32:37 +0900

    # 프로젝트 생성 기능 개발

--- src/components/project/NewProjectDialog.vue
+++ src/components/project/NewProjectDialog.vue
@@ -1,10 +1,14 @@
 <template>
-  <q-dialog v-model="isOpen" no-backdrop-dismiss no-esc-dismiss>
-    <q-card class="my-card" style="width: 700px;">
+  <q-dialog
+    v-model="isOpen"
+    no-backdrop-dismiss
+    no-esc-dismiss
+    @hide="dialogHide"
+  >
+    <q-card class="my-card" style="width: 500px;">
       <q-card-section class="row items-center q-py-xs">
         <div class="text-overline">New Project</div>
         <q-space />
-        <q-btn icon="crop_square" flat dense />
         <q-btn icon="close" flat dense v-close-popup />
       </q-card-section>
       <q-separator />
@@ -15,18 +19,21 @@
         <q-card-section>
           <q-input
             v-model="project.name"
-            label="프로젝트 이름"
+            label="Input project name"
             :rules="[ val => val && val.length > 0 || 'Please type something']"
           />
           <q-file
             :value="files"
+            label="Select Avatar image"
+            label-color="white"
             @input="updateFiles"
-            label="프로젝트 이미지 추가"
           >
             <template v-slot:before>
               <q-avatar square size="50px">
+                <q-icon v-if="isAvatar" name="far fa-image" />
                 <q-img
-                  :src="renderImg"
+                  v-else
+                  :src="project.avatar"
                 />
               </q-avatar>
             </template>
@@ -49,26 +56,42 @@ export default {
       isOpen: false,
       project: {
         name: '',
-        image: null,
+        avatar: '',
+        avatarName: '',
       },
       files: null,
-      renderImg: '',
+    }
+  },
+  computed: {
+    isAvatar () {
+      return this.project.avatar === ''
     }
   },
   methods: {
     onSubmit () {},
     updateFiles (newFiles) {
       const reader = new FileReader()
+
+      this.files = newFiles
+      this.project.avatarName = newFiles.name
+
       reader.readAsDataURL(newFiles)
-      // eslint-disable-next-line func-names
-      reader.onload = function () {
-        console.log(reader.result)
-        this.renderImg = reader.result
+
+      reader.onload = function onload () {
+        this.project.avatar = reader.result
       }.bind(this)
-      // eslint-disable-next-line func-names
-      reader.onerror = function (error) {
+
+      reader.onerror = function onerror (error) {
         console.log('Error: ', error)
       }
+    },
+    dialogHide () {
+      this.project = {
+        name: '',
+        avatar: '',
+        avatarName: '',
+      }
+      this.files = null
     },
   },
 }
